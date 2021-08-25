77c11157b723cc09f4981ee3eb7f4bf340e98360
iA$CE <46474674+kw-indie@users.noreply.github.com>
2021-08-26 00:19:33 +0300

    - fixed libs that were duplicated w/ diff versions if not explicitly added, and removed those that were not needed. - bumbed versions.

--- app/src/main/java/asceapps/weatheria/util/NetworkX.kt
+++ app/src/main/java/asceapps/weatheria/util/NetworkX.kt
@@ -17,13 +17,11 @@ import kotlinx.coroutines.withContext
 import java.net.InetSocketAddress
 import java.net.Socket
 
-@RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
-fun Context.onlineStatusFlow() = onlineStatusFlow(
-	getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-)
-
 private const val TIMEOUT = 5000
-private fun onlineStatusFlow(cm: ConnectivityManager) = callbackFlow {
+
+@RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
+fun Context.onlineStatusFlow() = callbackFlow {
+	val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 	val request = NetworkRequest.Builder()
 		.addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
 		.build()
